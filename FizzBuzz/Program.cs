0f4691e383c5c4042663e446bdcccc22cfaa0c55
ponhens <142414097+ponhens@users.noreply.github.com>
2024-02-19 14:11:08 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+var number = 0;
+
+for (int i = 1; i < 101; i++)
+{
+    number = i;
+
+
+
+    if (number % 5 == 0 && number % 3 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+}
